aaa42c8edf5d62e36061cb89f24dadeb686da219
David Simons <sharesindavid@gmail.com>
2017-02-02 04:29:58 +0000

    Finish chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,17 @@ class User < ApplicationRecord
   before_save {email.downcase!}
   validates(:name, 
             presence: true, 
-            length: {maximum:50}
+            length: {maximum: 50}
             )
   validates(:email, 
             presence: true, 
             length: {maximum: 255}, 
             uniqueness: { case_sensitive: false },
             format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+            )
+  has_secure_password
+  validates(:password,
+            presence: true,
+            length: {minimum: 6}
             )
 end
