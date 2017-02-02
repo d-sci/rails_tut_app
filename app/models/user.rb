0c99f4d70fd2bdb4a43c8797dd6a4e8e84c25ab7
David Simons <sharesindavid@gmail.com>
2017-02-02 02:33:04 +0000

    Add validation on user data

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  before_save {email.downcase!}
+  validates(:name, 
+            presence: true, 
+            length: {maximum:50}
+            )
+  validates(:email, 
+            presence: true, 
+            length: {maximum: 255}, 
+            uniqueness: { case_sensitive: false },
+            format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+            )
+end
